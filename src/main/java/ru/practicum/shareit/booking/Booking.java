f1ec38d3799f7552a6ae79c30243da5271e2eb9e
Fattakh <LostHaoses@yandex.ru>
2024-05-28 19:27:30 +0300

    sprint-14_2

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,15 +1,18 @@
 package ru.practicum.shareit.booking;
 
+
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "bookings")
@@ -18,97 +21,18 @@ public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(name = "start_date")
     private LocalDateTime start;
-
     @Column(name = "end_date")
     private LocalDateTime end;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id", nullable = false)
+    @ToString.Exclude
     private Item item;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "booker_id", nullable = false)
+    @ToString.Exclude
     private User booker;
-
     @Enumerated(value = EnumType.STRING)
     private BookingStatus status;
-
-    // Getters
-    public Long getId() {
-        return id;
-    }
-
-    public LocalDateTime getStart() {
-        return start;
-    }
-
-    public LocalDateTime getEnd() {
-        return end;
-    }
-
-    public Item getItem() {
-        return item;
-    }
-
-    public User getBooker() {
-        return booker;
-    }
-
-    public BookingStatus getStatus() {
-        return status;
-    }
-
-    // Setters
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setStart(LocalDateTime start) {
-        this.start = start;
-    }
-
-    public void setEnd(LocalDateTime end) {
-        this.end = end;
-    }
-
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
-    public void setBooker(User booker) {
-        this.booker = booker;
-    }
-
-    public void setStatus(BookingStatus status) {
-        this.status = status;
-    }
-
-    // toString
-    @Override
-    public String toString() {
-        return "Booking{" +
-                "id=" + id +
-                ", start=" + start +
-                ", end=" + end +
-                ", status=" + status +
-                '}';
-    }
-
-    // equals
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Booking booking = (Booking) o;
-        return Objects.equals(id, booking.id) && Objects.equals(start, booking.start) && Objects.equals(end, booking.end) && Objects.equals(item, booking.item) && Objects.equals(booker, booking.booker) && status == booking.status;
-    }
-
-    // hashCode
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, start, end, item, booker, status);
-    }
 }
